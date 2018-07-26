4347abfbd65c0fbb8e415eb8f549f0a516e99cda
trantrungnam <trantrungnam1997vn2@gmail.com>
2018-07-26 17:40:58 +0700

    update source

--- blockchain.js
+++ blockchain.js
@@ -12,7 +12,7 @@ const lastOf = list => list[list.length - 1]
 
 class Block {
 
-  constructor(index, previousHash, timestamp, data, nonce=0, hash='') {
+  constructor(index, previousHash, timestamp, data, nonce = 0, hash = '') {
     this.index = index
     this.previousHash = previousHash
     this.timestamp = timestamp
@@ -47,6 +47,9 @@ class Block {
     return block
   }
 }
+function sleep(time) {
+  return new Promise((resolve) => setTimeout(resolve, time))
+}
 
 
 class Server {
@@ -55,7 +58,6 @@ class Server {
     this.blocks = [Block.GENESIS]
     this.peers = {}
     this.state = {}
-
     this.peerServer = dgram.createSocket('udp4')
     this.peerServer.on('listening', this.onPeerServerListening.bind(this))
     this.peerServer.on('message', this.onPeerMessage.bind(this))
@@ -63,6 +65,7 @@ class Server {
     this.httpServer = express()
     this.httpServer.use(parser.json())
     this.httpServer.get('/peers', this.showPeers.bind(this))
+    this.httpServer.get('/accounts', this.createAccount.bind(this))
     this.httpServer.get('/blocks', this.showBlocks.bind(this))
     this.httpServer.post('/blocks', this.processBlocks.bind(this))
     this.httpServer.post('/transactions', this.processTransaction.bind(this))
@@ -83,7 +86,6 @@ class Server {
     )
 
     this.peerServer.setBroadcast(true)
-
     const message = new Buffer('hello')
     setInterval(_ => {
       this.peerServer.send(message, 0, message.length, address.port, '172.28.0.0')
@@ -101,53 +103,68 @@ class Server {
   showBlocks(req, resp) { resp.json(this.blocks) }
 
   processTransaction(req, resp) {
-
+    console.log("Access")
+    var transaction = { "from": req.body.from, "to": req.body.to, "amount": req.body.amount }
     // - Verify signature
-    // - Verify balance
+    if (Object.keys(this.state).indexOf(transaction.from)) {
 
-    // - Current block
-    // if (!this.currentBlock) {
-    //   this.currentBlock = Block.fromPrevious(Block.GENESIS)
-    // }
-    // - Add transaction to block
-    this.currentBlock.data.push(transaction)
+      // - Verify balance
+      if (this.state[transaction.from] >= transaction.amount) {
 
-    // Response
+        // - Current block
+        if (!this.currentBlock) {
+          this.currentBlock = Block.fromPrevious(Block.GENESIS)
+        }
+        else {
+          this.currentBlock = Block.fromPrevious(this.currentBlock)
+        }
+        this.currentBlock.data = transaction
 
-    // - Check if we have waited for 30 seconds
-    // - Proof-of-work
-    while (!this.currentBlock.hash.startsWith('000')) {
-      this.currentBlock.nonce += 1
-      this.currentBlock.hash = this.currentBlock.calculateHash()
+        // Notice 30s
+        sleep(30000).then(() => {
+          console.log("Just a moment")
+        })
+
+        // - Proof-of-work
+        while (!this.currentBlock.hash.startsWith('000')) {
+          this.currentBlock.nonce += 1
+          this.currentBlock.hash = this.currentBlock.calculateHash()
+        }
+
+        //Respone
+        this.peerServer.setBroadcast(true)
+        console.log(this.peerServer.address())
+        Object.keys(this.peers).forEach((address) => {
+          setInterval(_ => {
+            this.peerServer.send("Transaction", 0, "Transaction".length, 2346, address)
+          }, 1000)
+        })
+      }
     }
-
-    this.blocks.push(this.currentBlock)
-
-    Object.keys(this.peers).forEach(address => {
-      // POST /blocks
-    })
-
-    this.currentBlock = Block.fromPrevious(this.currentBlock)
   }
 
   processBlocks(req, resp) {
     // TODO
-    // block.hash.startsWith('000')
-    // block.hash === block.calculateHash()
-    // block.previousHash === this.blocks[block.index - 1].hash
-
-    // block.index > lastOf(this.blocks).index
-
+    block.hash.startsWith('000')
+    block.hash === block.calculateHash()
+    block.previousHash === this.blocks[block.index - 1].hash
+    block.index > lastOf(this.blocks).index
     this.blocks.push(block)
   }
 
   createAccount(req, resp) {
     // TODO
     // - Generate key pair based on password
+    var user = { "password": req.body.password }
+    var account = crypto.createECDH('secp256k1')
+    account.setPrivateKey(crypto.createHash('sha256').update(Buffer.from(user.password), 'utf-8').digest())
+    this.state[crypto.createHmac('sha256', account.getPublicKey()).digest('hex')] = 10
+    console.log(`Public key: ${account.getPublicKey('hex')}`)
+    console.log(`Private key: ${account.getPrivateKey('hex')}`)
+    console.log(this.state)
     // - Response
   }
 }
-
 
 exports.Block = Block
 exports.Server = Server
